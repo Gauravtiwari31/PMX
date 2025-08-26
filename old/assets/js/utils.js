b8a2a195e2cb9340eed83b32a70d79226c81abd7
Ujjwal Kumar <66829853+Victor-Jnr@users.noreply.github.com>
2025-08-26 19:55:09 +0530

    Removed old unnecessary data

--- old/assets/js/utils.js
+++
@@ -1,3 +0,0 @@
-const saveLS = (k, v) => localStorage.setItem(k, JSON.stringify(v));
-const loadLS = (k, d) => { try { return JSON.parse(localStorage.getItem(k)) ?? d; } catch { return d; } };
-function clsx(...xs){ return xs.filter(Boolean).join(' '); }
